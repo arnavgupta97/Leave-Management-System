028c009a017252a047f01df6e09f537884495f41
arnavgupta97 <46704468+arnavgupta97@users.noreply.github.com>
2019-01-18 10:58:56 +0530

    Updated DataSource.java

--- src/com/lms/datasource/DataSource.java
+++ src/com/lms/datasource/DataSource.java
@@ -6,7 +6,7 @@ import com.lms.entities.Leave;
 import com.lms.entities.Employee;
 
 public class DataSource {
-	public static HashMap<Integer, User> employeeList = new HashMap<>();
+	public static HashMap<Integer, Employee> employeeList = new HashMap<>();
 	public static HashMap<Integer, Leave> leaveList = new HashMap<>();
 	public static Employee currentLoggedInUser;
 }
